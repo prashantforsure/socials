561382599a55aeecf19088c0716f6d4fb3152e54
prashantforsure <prashantforsure@gmail.com>
2024-09-07 11:49:05 +0530

    firstcommit

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,17 @@
+import NextAuth from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID || '',
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET || ""
+      }),
+    // ...add more providers here
+    
+  ],
+}
+
+export default NextAuth(authOptions)
